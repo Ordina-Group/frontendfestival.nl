a0d822586929c3aff5945a0f020e166ba2a090df
Robbin "Roboroads" Schepers <robbin@deschepers.nl>
2024-07-16 16:34:00 +0200

    ci: Moven to static hosting

--- devops/main.tf
+++ devops/main.tf
@@ -53,78 +53,14 @@ resource "azurerm_static_web_app" "frontendfestival" {
   location            = azurerm_resource_group.frontendfestival.location
 }
 
-# resource "azurerm_static_web_app_custom_domain" "frontendfestival" {
-#   static_web_app_id = azurerm_static_web_app.frontendfestival.id
-#   domain_name       = "frontendfestival.nl"
-#   validation_type   = "cname-delegation"
-# }
-#
-# resource "azurerm_static_web_app_custom_domain" "wwwFrontendfestival" {
-#   static_web_app_id = azurerm_static_web_app.frontendfestival.id
-#   domain_name       = "www.frontendfestival.nl"
-#   validation_type   = "cname-delegation"
-# }
-
-resource "azurerm_service_plan" "frontendfestival" {
-  name                = "frontendfestival"
-  resource_group_name = azurerm_resource_group.frontendfestival.name
-  location            = azurerm_resource_group.frontendfestival.location
-  os_type             = "Linux"
-  sku_name            = "P0v3"
+resource "azurerm_static_web_app_custom_domain" "frontendfestival" {
+  static_web_app_id = azurerm_static_web_app.frontendfestival.id
+  domain_name       = "frontendfestival.nl"
+  validation_type   = "cname-delegation"
 }
 
-resource "azurerm_linux_web_app" "frontendfestival" {
-  name                = "frontendfestival"
-  resource_group_name = azurerm_resource_group.frontendfestival.name
-  location            = azurerm_service_plan.frontendfestival.location
-  service_plan_id     = azurerm_service_plan.frontendfestival.id
-
-  https_only = true
-
-  site_config {
-    always_on        = false
-    http2_enabled    = true
-    app_command_line = "pm2 start ecosystem.config.cjs --no-daemon"
-
-    application_stack {
-      node_version = "20-lts"
-    }
-  }
-}
-
-resource "azurerm_app_service_custom_hostname_binding" "frontendfestival" {
-  hostname            = "frontendfestival.nl"
-  app_service_name    = azurerm_linux_web_app.frontendfestival.name
-  resource_group_name = azurerm_resource_group.frontendfestival.name
-}
-
-resource "azurerm_app_service_custom_hostname_binding" "wwwFrontendfestival" {
-  hostname            = "www.frontendfestival.nl"
-  app_service_name    = azurerm_linux_web_app.frontendfestival.name
-  resource_group_name = azurerm_resource_group.frontendfestival.name
-}
-
-resource "azurerm_app_service_managed_certificate" "frontendfestival" {
-  custom_hostname_binding_id = azurerm_app_service_custom_hostname_binding.frontendfestival.id
-}
-resource "azurerm_app_service_managed_certificate" "wwwFrontendfestival" {
-  custom_hostname_binding_id = azurerm_app_service_custom_hostname_binding.wwwFrontendfestival.id
-}
-
-resource "azurerm_app_service_certificate_binding" "frontendfestival" {
-  hostname_binding_id = azurerm_app_service_custom_hostname_binding.frontendfestival.id
-  certificate_id      = azurerm_app_service_managed_certificate.frontendfestival.id
-  ssl_state           = "SniEnabled"
-}
-
-resource "azurerm_app_service_certificate_binding" "wwwFrontendfestival" {
-  hostname_binding_id = azurerm_app_service_custom_hostname_binding.wwwFrontendfestival.id
-  certificate_id      = azurerm_app_service_managed_certificate.wwwFrontendfestival.id
-  ssl_state           = "SniEnabled"
-}
-
-resource "azurerm_app_service_source_control" "frontendfestival" {
-  app_id   = azurerm_linux_web_app.frontendfestival.id
-  repo_url = "https://github.com/Ordina-Group/frontendfestival.nl"
-  branch   = "main"
+resource "azurerm_static_web_app_custom_domain" "wwwFrontendfestival" {
+  static_web_app_id = azurerm_static_web_app.frontendfestival.id
+  domain_name       = "www.frontendfestival.nl"
+  validation_type   = "cname-delegation"
 }
