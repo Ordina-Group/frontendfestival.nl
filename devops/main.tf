b386dcee53f38d0bea8920ce401faf3b539fa85f
Robbin "Roboroads" Schepers <robbin@deschepers.nl>
2024-07-16 15:58:39 +0200

    feat: App now builds statically

--- devops/main.tf
+++ devops/main.tf
@@ -6,12 +6,14 @@ terraform {
     }
   }
 
-  #   backend "azurerm" {
-  #     resource_group_name  = "frontendfestival"
-  #     storage_account_name = "storage-account"
-  #     container_name       = "tfstate"
-  #     key                  = "prod.terraform.tfstate"
-  #   }
+  # Comment this block when setting up the first time and init locally to create the storage account
+  # then, apply, uncomment, init again and apply again.
+  backend "azurerm" {
+    resource_group_name  = "frontendfestival"
+    storage_account_name = "frontendfestivalstorage"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -29,6 +31,39 @@ resource "azurerm_resource_group" "frontendfestival" {
     technical-contact = "Robbin Schepers <robbin.schepers@ordina.nl>"
   }
 }
+
+resource "azurerm_storage_account" "frontendfestival" {
+  name                     = "frontendfestivalstorage"
+  resource_group_name      = azurerm_resource_group.frontendfestival.name
+  location                 = azurerm_resource_group.frontendfestival.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_nested_items_to_be_public = false
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.frontendfestival.name
+  container_access_type = "private"
+}
+
+resource "azurerm_static_web_app" "frontendfestival" {
+  name                = "frontendfestival"
+  resource_group_name = azurerm_resource_group.frontendfestival.name
+  location            = azurerm_resource_group.frontendfestival.location
+}
+
+# resource "azurerm_static_web_app_custom_domain" "frontendfestival" {
+#   static_web_app_id = azurerm_static_web_app.frontendfestival.id
+#   domain_name       = "frontendfestival.nl"
+#   validation_type   = "cname-delegation"
+# }
+#
+# resource "azurerm_static_web_app_custom_domain" "wwwFrontendfestival" {
+#   static_web_app_id = azurerm_static_web_app.frontendfestival.id
+#   domain_name       = "www.frontendfestival.nl"
+#   validation_type   = "cname-delegation"
+# }
 
 resource "azurerm_service_plan" "frontendfestival" {
   name                = "frontendfestival"
