b9e87a1a05dc41559f9d47b1828d647651f10898
Hamza Haiken <tenchi@team2xh.net>
2025-07-09 11:30:21 +0200

    Fix wrong label for presentation video

--- src/routes/InfoSection.svelte
+++ src/routes/InfoSection.svelte
@@ -14,7 +14,7 @@
 	<div class="pt-10 mb-10 flex-center">
 		<Video
 			src="/AftermoviePycon2025.mp4"
-			message="Watch the PyCon 2025 Presentation"
+			message="Watch the PyCon 2024 Presentation"
 			class="center max-h-[480px]"
 		/>
 	</div>
